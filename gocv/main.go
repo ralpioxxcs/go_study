61b0db9cb597d943a4e3291da643be4c3aeaa45a
ralpioxxcs <tmfmfkal1123@naver.com>
2020-11-22 14:23:23 +0900

    [UPDATE] videocapture example

--- gocv/main.go
+++ gocv/main.go
@@ -1,75 +1,52 @@
 package main
 
-import(
-  "fmt"
+import (
+	"fmt"
   "image"
-  "image/color"
-  "os"
-  "strconv"
 
-  "gocv.io/x/gocv"
+	"gocv.io/x/gocv"
 )
 
+func open(id int) (vc* gocv.VideoCapture, err error) {
+  vc, err = gocv.VideoCaptureDevice(id)
+  return vc, err
+}
+
 func main() {
-  fmt.Println("running cv example ...")
 
-  if len(os.Args) < 3 {
-    fmt.Println("How to run:\n\tfacedetect [camID] [XML]")
-    return
-  }
+  fmt.Println("Video capturing")
 
+  //webcam, _ := open(0)
 
-  deviceID, _ := strconv.Atoi(os.Args[1])
-  xmlFile := os.Args[2]
-
-  webcamm, err := gocv.VideoCaptureDevice(int(deviceID))
+  webcam, err := gocv.VideoCaptureDevice(0)
   if err != nil {
     fmt.Println(err)
-    return
+    panic(err)
   }
-  defer webcamm.Close()
+  defer webcam.Close()
 
-  window := gocv.NewWindow("Face Detect")
-  defer window.Close()
+  window := gocv.NewWindow("image")
+  window.ResizeWindow(640, 480)
+  //defer window.Close()
 
-  img := gocv.NewMat();
-  defer img.Close()
-
-  blue := color.RGBA{0,0,255,0}
-
-  classifier := gocv.NewCascadeClassifier()
-  defer classifier.Close()
-
-  if !classifier.Load(xmlFile) {
-    fmt.Printf("Error reading cacade file: %v\n", xmlFile)
-    return
-  }
-
-  fmt.Println("start reading camera device : %v\n", deviceID)
+  img := gocv.NewMat()
+  
   for {
-    if ok := webcamm.Read(&img); !ok {
-      fmt.Printf("can't read device %d\n", deviceID)
+    if ok := webcam.Read(&img); !ok {
+      fmt.Sprintln("can't read camera device")
       return
     }
     if img.Empty() {
       continue
     }
 
-    rects := classifier.DetectMultiScale(img)
-    fmt.Printf("found %d faces\n", len(rects))
-
-    for _, r := range rects {
-      gocv.Rectangle(&img, r, blue, 3)
-
-      size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
-      pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2),r.Min.Y-2)
-      gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
-    }
+    gocv.Resize(img, &img, image.Point{0, 0}, 0.5, 0.5, gocv.InterpolationArea)
 
     window.IMShow(img)
     if window.WaitKey(1) >= 0 {
+      fmt.Println("image show break")
       break
     }
-
   }
+
 }
