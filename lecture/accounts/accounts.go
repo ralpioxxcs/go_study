4233ad73f8da92a0c43df4dc3403a79d0d9c128b
ralpioxxcs <tmfmfkal1123@naver.com>
2020-11-24 23:51:15 +0900

    [UPDATE] files

--- lecture/accounts/accounts.go
+++ lecture/accounts/accounts.go
@@ -1,5 +1,9 @@
 package accounts 
 
+import "errors"
+
+var errNoMoney = errors.New("Can't withdraw you are poor!")
+
 // Account struct
 type Account struct {
   owner   string
@@ -12,4 +16,23 @@ func NewAccount(owner string) *Account{
   return &account
 }
 
+// Deposit x amount on your account
+// don't copy of account ,, use address
+func (a *Account) Deposit(amount int) {
+  a.balance += amount
+}
 
+// Balance return balance on your account
+func (a Account) Balance() int {
+  return a.balance
+}
+
+// Withdraw x amount from on your account
+func (a *Account) Withdraw(amount int) error {
+  if a.balance < amount {
+    //return errors.New("Can'y withdraw you are poor!")
+    return errNoMoney
+  }
+  a.balance -= amount
+  return nil
+}
--- lecture/main.go
+++ lecture/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
   "fmt"
+  "log"
 
   "github.com/ralpioxxcs/go_study/lecture/accounts"
 )
 
 func main() {
   account := accounts.NewAccount("ralpio")
+  account.Deposit(500)
+  fmt.Println(account)
+
+  err := account.Withdraw(600)
+  if err != nil {
+    log.Fatalln(err)
+  }
   fmt.Println(account)
 }
 
